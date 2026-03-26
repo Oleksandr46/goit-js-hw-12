c9a5def2e396468f05a0bd9f9e3997ea9b0a20ee
Oleksandr Melnyk <melnykoleksandr724@gmail.com>
2026-03-26 21:03:22 +0100

    render main.js

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -3,7 +3,7 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const refs = {
   gallery: document.querySelector('.gallery'),
-  loader: document.querySelector('.loader'),
+  loader: document.querySelector('.loader-wrapper'),
   loadMore: document.querySelector('.load-more'),
 };
 
@@ -11,77 +11,61 @@ const lightbox = new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
 });
 
+function createMarkup(images) {
+  return images
+    .map(
+      ({
+        largeImageURL,
+        webformatURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) =>
+        `<li class="gallery-item">
+            <a href="${largeImageURL}">
+              <img class="gallery-img" src="${webformatURL}" alt="${tags}" loading="lazy"/>
+            </a>
+            <div class="gallery-wrapper">
+              <p class="wrapper-list"><b>likes:</b> ${likes}</p>
+              <p class="wrapper-list"><b>views:</b> ${views}</p>
+              <p class="wrapper-list"><b>comments:</b> ${comments}</p>
+              <p class="wrapper-list"><b>downloads:</b> ${downloads}</p>
+            </div>
+          </li>`
+    )
+    .join('');
+}
 export function createGallery(images) {
-  const markup = images
-    .map(
-      ({
-        largeImageURL,
-        webformatURL,
-        tags,
-        likes,
-        views,
-        comments,
-        downloads,
-      }) =>
-        `<li class="gallery-item">
-            <a href="${largeImageURL}">
-              <img class="gallery-img" src="${webformatURL}" alt="${tags}" loading="lazy"/>
-            </a>
-            <div class="gallery-wrapper">
-              <p class="wrapper-list"><b>likes:</b> ${likes}</p>
-              <p class="wrapper-list"><b>views:</b> ${views}</p>
-              <p class="wrapper-list"><b>comments:</b> ${comments}</p>
-              <p class="wrapper-list"><b>downloads:</b> ${downloads}</p>
-            </div>
-          </li>`
-    )
-    .join('');
-  refs.gallery.innerHTML = markup;
+  if (!refs.gallery) return;
+
+  refs.gallery.innerHTML = createMarkup(images);
   lightbox.refresh();
 }
-
 export function appendGallery(images) {
-  const markup = images
-    .map(
-      ({
-        largeImageURL,
-        webformatURL,
-        tags,
-        likes,
-        views,
-        comments,
-        downloads,
-      }) =>
-        `<li class="gallery-item">
-            <a href="${largeImageURL}">
-              <img class="gallery-img" src="${webformatURL}" alt="${tags}" loading="lazy"/>
-            </a>
-            <div class="gallery-wrapper">
-              <p class="wrapper-list"><b>likes:</b> ${likes}</p>
-              <p class="wrapper-list"><b>views:</b> ${views}</p>
-              <p class="wrapper-list"><b>comments:</b> ${comments}</p>
-              <p class="wrapper-list"><b>downloads:</b> ${downloads}</p>
-            </div>
-          </li>`
-    )
-    .join('');
-  refs.gallery.insertAdjacentHTML('beforeend', markup);
+  if (!refs.gallery) return;
+
+  refs.gallery.insertAdjacentHTML('beforeend', createMarkup(images));
   lightbox.refresh();
 }
-
 export function clearGallery() {
+  if (!refs.gallery) return;
   refs.gallery.innerHTML = '';
 }
 export function showLoader() {
+  if (!refs.loader) return;
   refs.loader.classList.add('visible');
 }
 export function hideLoader() {
+  if (!refs.loader) return;
   refs.loader.classList.remove('visible');
 }
-
 export function showLoadMoreButton() {
+  if (!refs.loadMore) return;
   refs.loadMore.classList.add('visible');
 }
 export function hideLoadMoreButton() {
+  if (!refs.loadMore) return;
   refs.loadMore.classList.remove('visible');
 }
--- src/main.js
+++ src/main.js
@@ -5,14 +5,20 @@ import {
   showLoader,
   createGallery,
   hideLoader,
+  appendGallery,
+  showLoadMoreButton,
+  hideLoadMoreButton,
 } from './js/render-functions';
 import { getImagesByQuery } from './js/pixabay-api';
 
 const refs = {
   form: document.querySelector('.form'),
+  loadMore: document.querySelector('.load-more'),
 };
+let currentQuery = '';
+let currentPage = 1;
 
-refs.form.addEventListener('submit', e => {
+refs.form.addEventListener('submit', async e => {
   e.preventDefault();
   const query = e.target.elements['search-text'].value.trim();
 
@@ -22,28 +28,57 @@ refs.form.addEventListener('submit', e => {
     });
     return;
   }
-  showLoader();
+  currentQuery = query;
+  currentPage = 1;
+
   clearGallery();
-
-  getImagesByQuery(query)
-    .then(data => {
-      if (!data?.hits?.length) {
-        iziToast.error({
-          message:
-            'Sorry, there are no images matching your search query. Please try again!',
-        });
-        return;
-      }
-      e.target.reset();
-
-      createGallery(data.hits);
-    })
-    .catch(error => {
+  showLoader();
+  hideLoadMoreButton();
+  try {
+    const data = await getImagesByQuery(currentQuery, currentPage);
+    const totalPages = Math.ceil(data.totalHits / 15);
+    if (currentPage < totalPages) {
+      showLoadMoreButton();
+    } else {
+      hideLoadMoreButton();
+    }
+    if (!data?.hits?.length) {
       iziToast.error({
-        message: 'Something went wrong. Please try again later.',
+        message:
+          'Sorry, there are no images matching your search query. Please try again!',
       });
-    })
-    .finally(() => {
-      hideLoader();
+      return;
+    }
+
+    createGallery(data.hits);
+    currentPage += 1;
+    e.target.reset();
+  } catch (error) {
+    iziToast.error({
+      message: 'Something went wrong. Please try again later.',
     });
+  } finally {
+    hideLoader();
+  }
+});
+refs.loadMore.addEventListener('click', async e => {
+  e.preventDefault();
+
+  showLoader();
+  hideLoadMoreButton();
+
+  const data = await getImagesByQuery(currentQuery, currentPage);
+  appendGallery(data.hits);
+
+  const totalPages = Math.ceil(data.totalHits / 15);
+  if (currentPage < totalPages) {
+    showLoadMoreButton();
+  } else {
+    hideLoadMoreButton();
+  }
+  iziToast.info({
+    message: "We're sorry, but you've reached the end of search results",
+  });
+
+  currentPage += 1;
 });
